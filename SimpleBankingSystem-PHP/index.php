f4b7a69cb93aa0b1a064d6b09289bc0cd8feb422
HEMANTH_NANDURI <104613494+hemanth0596@users.noreply.github.com>
2022-09-14 21:39:40 +0530

    Add files via upload

---
+++ SimpleBankingSystem-PHP/index.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+  <head><center>
+  <a class="navbar-brand" href="index.php" style="color : #495057;"><img src="img/bnklg.jpg" alt="Logo" width="77" style="margin-bottom:4px;"> <b>SPARK'S BANKING SYSTEM</b></a>
+    </center>
+	<!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+
+    <title>Basic Banking System</title>
+  </head>
+
+  <body>
+  <?php
+  include 'navbar.php';
+  ?>
+  
+      <div class="container-fluid">
+      <!-- Introduction section -->
+            <div class="row intro py-1" style="background-color : #C7FF33;">
+              
+              <div class="col-sm-12 col-md img text-center">
+                <img src="img/bank.jpg" >
+              </div>
+            </div>
+
+      <!-- Activity section -->
+            <div class="row activity text-center">
+                 
+                 
+                  <div class="col-md act">
+                    <img src="img/transfer.png" class="img-fluid">
+                    <br>
+                    <a href="transfermoney.php"><button style="background-color : #2785C4;">Make a Transaction</button></a>
+                  </div>
+                  <div class="col-md act">
+                    <img src="img/history.png" class="img-fluid" style="width:180px;height:195px;">
+                    <br>
+                    <a href="transactionhistory.php"><button style="background-color : #2785C4;">Transaction History</button></a>
+                  </div>
+                  
+            </div>
+      </div>
+      
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+  </body>
+</html>
